dbf628cec23bbd5938459ec6a6de83e60875af6f
NTT-DevFPT <thanhtai10903@gmail.com>
2024-12-10 20:31:24 +0700

    Add ecommerce-fe folder

---
+++ ecommerce-be/src/test/java/com/xiaomi/ecommercefe/EcommerceFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaomi.ecommercefe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
